f6030955b3c353cbf376ad1b82e2fbdf41451b44
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-04 19:15:42 +0300

    correct registr username

--- Example006_ConditionIfElse/Program.cs
+++ Example006_ConditionIfElse/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.WriteLine("What you name? ");
 string username = Console.ReadLine();
 
-if (username == "Anna")
-{
+if (username.ToLower() == "anna")  // операция ToLower() позволяет все символы 
+{                                  // в нижний регистр
     Console.WriteLine("I love you");
 }
 else
 {
     Console.WriteLine("Hello!");
+    Console.WriteLine(username); // added
 }
