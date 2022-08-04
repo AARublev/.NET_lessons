f7eaea9f8a7f8965317057643bcc5a684a0e85da
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-04 19:06:18 +0300

    added Example 006 Condition is --> else

---
+++ Example006_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("What you name? ");
+string username = Console.ReadLine();
+
+if (username == "Anna")
+{
+    Console.WriteLine("I love you");
+}
+else
+{
+    Console.WriteLine("Hello!");
+}
