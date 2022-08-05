6079ff9e4093cb85cf6b79fee92b59af2cad9b03
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-05 11:19:07 +0300

    Example 009 sqrt

---
+++ Example009_sqrt/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sqrt = number*number;
+Console.WriteLine($"sqrt number: {number} = {sqrt}"); // $ вывоз значений переменных в консоль
+
+
