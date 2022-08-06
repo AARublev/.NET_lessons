c861fab80a174abeb811b639c7c9f34b4169e6d6
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-06 18:02:40 +0300

    Example 12 number is even

---
+++ Example012_honestNum/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Definition the number is even");
+Console.Write("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2 == 0)
+{
+    Console.WriteLine("Yes, this number is even");
+}
+else
+    Console.WriteLine("No, this number is not even");
