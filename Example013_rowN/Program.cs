b69c24c6e3b5d282a8ece2a8238670d19f2d2b4c
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-06 19:45:42 +0300

    Example 13 a series of even numbers

---
+++ Example013_rowN/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("a series of even numbers");
+Console.WriteLine("enter a natural number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = num1 - num1 + 1;
+
+if (num1>0)
+{
+while (num2<=num1)
+{
+   if (num2 % 2 == 0)
+   {
+      Console.Write($"{num2} ");
+   }
+   num2+=1;
+}
+Console.Write("\n");
+}
+else
+   Console.WriteLine("enter a natural number");
