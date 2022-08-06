df1289beb16cb7133f2c2831e773f41d846bb0ca
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-06 17:19:40 +0300

    added Example 10 conlision max and min numbers

---
+++ Example010_min&max/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1<num2)
+{
+    Console.WriteLine($"max number: {num2} \nmin number: {num1}");
+}
+else
+    Console.WriteLine($"max number: {num1} \nmin number: {num2}");
