4c8459c81e4403ce6d4400a7eeff0f175f01bc81
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-08 16:37:16 +0300

    added Example 16 methods array. Index search

---
+++ Example016_MethodsArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,23,4,6,3,2,4,55,43,323, 55};
+int n = array.Length;   //возвращает кол-во элементов массива или его длину. Т.е. n=длина array 
+int find = 55;
+
+int index=0;
+while (index<n)
+{
+    if (array[index]==find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
