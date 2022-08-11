0835c2ee8bb82d0a2ecbca86e2d261f51bb7bd50
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-11 17:39:23 +0300

    Added Example - calendar

---
+++ Example026_Calendar/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number of the day of the week: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Calend(int num)
+{
+    return num==6 || num==7; //rtue
+}
+
+if (Calend(number)) Console.WriteLine("Yes");
+else Console.WriteLine("No");
