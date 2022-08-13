d9d7fb9337df5eec6585bc580b70e4b1e4cbad98
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-13 16:20:51 +0300

    added practice 3 - four example

---
+++ practiсe_3/Example028_defineValues/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите четверть от 1 до 4: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string GetQuater(int num)
+{
+    if (num == 1) return "X>0 и Y>0";
+    if (num == 2) return "X<0 и Y>0"; //else не нужен т.к. return прерывает
+    if (num == 3) return "X<0 и Y<0"; //else не нужен т.к. return прерывает
+    if (num == 4) return "X>0 и Y<0"; //else не нужен т.к. return прерывает
+    else return "Некоректный ввод, введите от 1 до 4";
+}
+
+string result = GetQuater(number);
+Console.WriteLine(result);
