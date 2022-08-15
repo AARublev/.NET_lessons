dd0620ecc9a6a3600cd5fbbe30773ebbd06f2889
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-15 22:52:42 +0300

    rename practiсe3

---
+++ practiсe3/Example30_TableSqrt/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void QuardNum(int num)
+{
+    int i = 1;
+    while (i <= num)
+    {
+        int quad = i * i;
+        Console.WriteLine($"{i} | {quad}");
+        Console.WriteLine("--------");
+        i++;
+    }
+}
+
+QuardNum(number);
