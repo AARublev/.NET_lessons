0f2ba07b331c1fb445cddbd62a477c9f022fec3b
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-26 00:39:48 +0300

    added homework number 6

---
+++ Homework6/Example070_scorePositiveNumArray/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Enter any number of digits. Enter 606 to stop");
+
+int number;
+int[] startArray = new int[] { };
+
+while (true)
+{
+    Console.Write("Enter number: ");
+    number = Convert.ToInt32(Console.ReadLine());
+    if (number == 606) break;
+    else startArray = startArray.Concat(new int[] { number }).ToArray(); //ToArray() - копирует элементы в новый массив
+}                                                                        //Concat - метод объеденмет элементы массива
+
+int ScorePositiveNumArray(int[] array)
+{
+    int sum=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]>0) sum++;
+    }
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[^1]}");
+}
+
+int resul = ScorePositiveNumArray(startArray);
+
+PrintArray(startArray);
+Console.WriteLine($" --> {resul}");
---
+++ Homework6/Example071_/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+//2 прямые могут:
+//быть паралельны
+//совподать
+//пересекаться в единственной точке - для этой программы исключающее исловие для первых 2-х случаев
+
+Console.Write("Enter b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+void IntersectionPoint(double bb1, double kk1, double bb2, double kk2)
+{
+    double x = 0;
+    double y1 = 0;
+    double y2 = 0;
+
+    x = (bb2 - bb1) / (kk1 - kk2);
+    y1 = kk1 * x + bb1;
+    y2 = kk2 * x + bb2;
+
+    //коэффициенты для проверки совпадения прямых
+    double l1 = bb2 / bb1;
+    double l2 = kk2 / kk1; 
+    double l3 = y2 / y1; 
+
+    if (l1==l2 && l1==l3) Console.WriteLine("straight lines match");
+    if (kk1==kk2 && y2==y1 && bb2!=bb1) Console.WriteLine("parallel lines");
+    else Console.WriteLine($"({y1}; {y2})");
+}
+
+IntersectionPoint(b1, k1, b2, k2);
