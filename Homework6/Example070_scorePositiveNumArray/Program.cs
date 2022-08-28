e3a53c267e5f58290aaaceaf3af834581b5c7c37
Aleksey Rublev <AAlekseyRublev@gmail.com>
2022-08-28 11:35:56 +0300

    a new way to add to an array

--- Homework6/Example070_scorePositiveNumArray/Program.cs
+++ Homework6/Example070_scorePositiveNumArray/Program.cs
@@ -6,22 +6,33 @@
 Console.WriteLine("Enter any number of digits. Enter 606 to stop");
 
 int number;
-int[] startArray = new int[] { };
+//int[] startArray = new int[] { };
+var userNumberList = new List<int>();
+int stop = 606;
+
+// while (true)
+// {
+//     Console.Write("Enter number: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+//     if (number == 606) break;
+//     else startArray = startArray.Concat(new int[] { number }).ToArray(); //ToArray() - копирует элементы в новый массив
+// }                                                                        //Concat - метод объеденмет элементы массива
 
 while (true)
 {
     Console.Write("Enter number: ");
     number = Convert.ToInt32(Console.ReadLine());
-    if (number == 606) break;
-    else startArray = startArray.Concat(new int[] { number }).ToArray(); //ToArray() - копирует элементы в новый массив
-}                                                                        //Concat - метод объеденмет элементы массива
+    if (number == stop) break;
+    else userNumberList.Add(number); //пишем number в список
+}
+int[] startArray = userNumberList.ToArray(); //копирует список в массив
 
 int ScorePositiveNumArray(int[] array)
 {
-    int sum=0;
+    int sum = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i]>0) sum++;
+        if (array[i] > 0) sum++;
     }
     return sum;
 }
